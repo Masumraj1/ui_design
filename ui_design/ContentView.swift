faa452f5d98ad7f5c4510c2e01a9f8db8f998ae7
masum <promotionbd321@gmail.com>
2025-07-24 15:38:41 +0600

    some changes

--- ui_design/ContentView.swift
+++ ui_design/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, ")
-        }
-        .padding()
+        
+        ZStack {
+                   Color.white
+                .ignoresSafeArea()
+
+                   VStack {
+                       Image(systemName: "globe")
+                           .imageScale(.medium)
+                           .foregroundStyle(.yellow)
+                       Text("Hello, ")
+                   }
+                   .padding()
+                   .background(.tint)
+               }
+        
     }
 }
 
